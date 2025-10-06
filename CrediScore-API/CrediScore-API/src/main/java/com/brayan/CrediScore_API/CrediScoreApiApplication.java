f2e8fc49bd097bc272f3d64bcfcd922f048303e5
Brayan Díaz <brayandiaz258f@gmail.com>
2025-10-05 22:23:41 -0500

    Cargando estructura del proyecto

---
+++ CrediScore-API/CrediScore-API/src/main/java/com/brayan/CrediScore_API/CrediScoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.brayan.CrediScore_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrediScoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrediScoreApiApplication.class, args);
+	}
+
+}
